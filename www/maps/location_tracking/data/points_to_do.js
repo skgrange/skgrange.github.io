0d1317f1d9f9af96fc08688235b56004499e3623
Stuart Grange <s.k.grange@gmail.com>
2022-10-30 17:16:01 +0100

    commit all website changes with a script

--- www/maps/location_tracking/data/points_to_do.js
+++ www/maps/location_tracking/data/points_to_do.js
@@ -2112,6 +2112,30 @@ var points_to_do = [{
         "name": "Panlong Ancient Road",
         "type": "To-do"
       }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [8.57807, 47.55503]
+      },
+      "properties": {
+        "date": "",
+        "name": "Hörnli",
+        "type": "To-do"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [8.59369, 47.556]
+      },
+      "properties": {
+        "date": "",
+        "name": "Grosser Hebelstein",
+        "type": "To-do"
+      }
     }
   ]
 }];
