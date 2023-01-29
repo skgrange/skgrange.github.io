25507448a4c34d1358ec9d57a05b200df9417002
Stuart Grange <s.k.grange@gmail.com>
2023-01-29 14:58:55 +0100

    commit all website changes with a script

--- www/maps/location_tracking/data/points_done.js
+++ www/maps/location_tracking/data/points_done.js
@@ -32849,6 +32849,18 @@ var points_done = [{
       "type": "Feature",
       "geometry": {
         "type": "Point",
+        "coordinates": [-2.2764, 53.36672]
+      },
+      "properties": {
+        "date": "2023-01-28",
+        "name": "Ibis Budget",
+        "type": "Hotel"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
         "coordinates": [-0.59075, 51.51056]
       },
       "properties": {
