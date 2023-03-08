ce6eb77642db7b190f867db1d9a4cf822723de2c
Stuart Grange <s.k.grange@gmail.com>
2023-03-09 12:19:02 +1300

    commit all website changes with a script

--- www/maps/location_tracking/data/points_done.js
+++ www/maps/location_tracking/data/points_done.js
@@ -33821,6 +33821,18 @@ var points_done = [{
       "type": "Feature",
       "geometry": {
         "type": "Point",
+        "coordinates": [174.09198, -35.28063]
+      },
+      "properties": {
+        "date": "2023-03-09",
+        "name": "Zane Grey's Restaurant and Bar",
+        "type": "Coffee"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
         "coordinates": [-0.59075, 51.51056]
       },
       "properties": {
