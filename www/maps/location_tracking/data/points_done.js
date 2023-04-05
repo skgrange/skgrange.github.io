810ce1210f0eac2cfd307fa40a1f66cdd7ef1e02
skgrange <s.k.grange@gmail.com>
2023-04-05 13:51:43 +0200

    commit all website changes with a script

--- www/maps/location_tracking/data/points_done.js
+++ www/maps/location_tracking/data/points_done.js
@@ -34301,6 +34301,30 @@ var points_done = [{
       "type": "Feature",
       "geometry": {
         "type": "Point",
+        "coordinates": [8.58253, 47.47204]
+      },
+      "properties": {
+        "date": "2023-04-05",
+        "name": "Egetswil",
+        "type": "Attraction"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [8.57502, 47.48413]
+      },
+      "properties": {
+        "date": "2023-04-05",
+        "name": "Lufingerstrasse",
+        "type": "Attraction"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
         "coordinates": [-0.59075, 51.51056]
       },
       "properties": {
