6eb08f3e9954eb359acc941e12a8931e8765ccc3
Stuart Grange <s.k.grange@gmail.com>
2023-07-04 20:16:20 +0200

    commit all website changes with a script

--- www/maps/location_tracking/data/points_done.js
+++ www/maps/location_tracking/data/points_done.js
@@ -36545,6 +36545,18 @@ var points_done = [{
       "type": "Feature",
       "geometry": {
         "type": "Point",
+        "coordinates": [8.55658, 47.35557]
+      },
+      "properties": {
+        "date": "2023-07-02",
+        "name": "Seefeld",
+        "type": "Attraction"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
         "coordinates": [-0.59075, 51.51056]
       },
       "properties": {
