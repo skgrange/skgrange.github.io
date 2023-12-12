fb9c1c08e8f41c9959d923e6b7ee9854d4060cce
Stuart Grange <s.k.grange@gmail.com>
2023-12-12 19:30:39 +0100

    commit all website changes with a script

--- www/maps/location_tracking/data/points_done.js
+++ www/maps/location_tracking/data/points_done.js
@@ -39677,6 +39677,18 @@ var points_done = [{
       "type": "Feature",
       "geometry": {
         "type": "Point",
+        "coordinates": [8.81863, 47.30646]
+      },
+      "properties": {
+        "date": "2023-12-11",
+        "name": "Hinwil Center",
+        "type": "Other"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
         "coordinates": [-0.59075, 51.51056]
       },
       "properties": {
