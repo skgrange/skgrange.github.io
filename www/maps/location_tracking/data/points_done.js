1f67c8c126fccc372ad0a3cc5ef22e3edd21ef0c
skgrange <skgrange@gmail.com>
2024-08-21 12:56:08 +0200

    commit all website changes with a script

--- www/maps/location_tracking/data/points_done.js
+++ www/maps/location_tracking/data/points_done.js
@@ -45401,6 +45401,18 @@ var points_done = [{
       "type": "Feature",
       "geometry": {
         "type": "Point",
+        "coordinates": [8.529808, 47.38243]
+      },
+      "properties": {
+        "date": "2024-08-21",
+        "name": "flavour",
+        "type": "Coffee"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
         "coordinates": [-0.59075, 51.51056]
       },
       "properties": {
