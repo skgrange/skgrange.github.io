dbd5ae4bf76cd6b9971c9040465dbd688e442ed4
skgrange <s.k.grange@gmail.com>
2025-01-11 18:54:54 +1000

    commit all website changes with a script

--- www/maps/location_tracking/data/points_done.js
+++ www/maps/location_tracking/data/points_done.js
@@ -49229,6 +49229,18 @@ var points_done = [{
       "type": "Feature",
       "geometry": {
         "type": "Point",
+        "coordinates": [153.0344, -27.48754]
+      },
+      "properties": {
+        "date": "2025-01-11",
+        "name": "Superfly Pizza",
+        "type": "Food"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
         "coordinates": [-0.59075, 51.51056]
       },
       "properties": {
