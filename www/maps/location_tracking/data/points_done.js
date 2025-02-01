09cdc8740909b37d935226b71fe28152813a82fb
skgrange <s.k.grange@gmail.com>
2025-02-01 16:27:34 +1000

    commit all website changes with a script

--- www/maps/location_tracking/data/points_done.js
+++ www/maps/location_tracking/data/points_done.js
@@ -50249,6 +50249,18 @@ var points_done = [{
       "type": "Feature",
       "geometry": {
         "type": "Point",
+        "coordinates": [153.0194, -27.4641]
+      },
+      "properties": {
+        "date": "2025-02-01",
+        "name": "The Garden Room",
+        "type": "Coffee"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
         "coordinates": [-0.59075, 51.51056]
       },
       "properties": {
