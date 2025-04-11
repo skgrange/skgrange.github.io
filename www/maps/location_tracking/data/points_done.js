4cfb2172fad7b6e169ca4d25a510ac7765fc720b
skgrange <s.k.grange@gmail.com>
2025-04-11 12:37:35 +1000

    commit all website changes with a script

--- www/maps/location_tracking/data/points_done.js
+++ www/maps/location_tracking/data/points_done.js
@@ -51893,6 +51893,18 @@ var points_done = [{
       "type": "Feature",
       "geometry": {
         "type": "Point",
+        "coordinates": [152.8914, -27.3929]
+      },
+      "properties": {
+        "date": "2025-04-11",
+        "name": "Camp Mountain Road junction",
+        "type": "Attraction"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
         "coordinates": [-0.59075, 51.51056]
       },
       "properties": {
