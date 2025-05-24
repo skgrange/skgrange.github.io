dd8ecf72964bf39b54c45c54add8ebe0501db6e5
Stuart Grange <s.k.grange@gmail.com>
2025-05-24 19:37:21 +1000

    commit all website changes with a script

--- www/maps/location_tracking/data/points_done.js
+++ www/maps/location_tracking/data/points_done.js
@@ -53585,6 +53585,18 @@ var points_done = [{
       "type": "Feature",
       "geometry": {
         "type": "Point",
+        "coordinates": [153.0903, -26.39663]
+      },
+      "properties": {
+        "date": "2025-05-24",
+        "name": "Monkey Punch",
+        "type": "Food"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
         "coordinates": [-0.59075, 51.51056]
       },
       "properties": {
