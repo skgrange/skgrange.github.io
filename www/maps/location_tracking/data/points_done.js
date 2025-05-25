5cf5101aa533ecc2d887772707a3ae0c63f52617
skgrange <s.k.grange@gmail.com>
2025-05-25 12:28:08 +1000

    commit all website changes with a script

--- www/maps/location_tracking/data/points_done.js
+++ www/maps/location_tracking/data/points_done.js
@@ -53597,6 +53597,18 @@ var points_done = [{
       "type": "Feature",
       "geometry": {
         "type": "Point",
+        "coordinates": [153.1298, -26.72543]
+      },
+      "properties": {
+        "date": "2025-05-25",
+        "name": "Bfresh",
+        "type": "Food"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
         "coordinates": [-0.59075, 51.51056]
       },
       "properties": {
