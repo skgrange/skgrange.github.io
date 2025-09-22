d66265c82d3fb09fc17a6c0e6dbd9e2fd86520e1
skgrange <s.k.grange@gmail.com>
2025-09-22 14:35:34 +0200

    commit all website changes with a script

--- www/maps/location_tracking/data/points_done.js
+++ www/maps/location_tracking/data/points_done.js
@@ -57377,6 +57377,18 @@ var points_done = [{
       "type": "Feature",
       "geometry": {
         "type": "Point",
+        "coordinates": [7.438149, 46.95042]
+      },
+      "properties": {
+        "date": "2025-09-22",
+        "name": "Haputgebäude",
+        "type": "Attraction"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
         "coordinates": [-0.59075, 51.51056]
       },
       "properties": {
