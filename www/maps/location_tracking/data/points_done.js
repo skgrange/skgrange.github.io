d11f3df610f6b57b7286e9f239f3e2d6ae81c4ee
skgrange <s.k.grange@gmail.com>
2026-01-25 14:12:52 +0100

    commit all website changes with a script

--- www/maps/location_tracking/data/points_done.js
+++ www/maps/location_tracking/data/points_done.js
@@ -62081,6 +62081,18 @@ var points_done = [{
       "type": "Feature",
       "geometry": {
         "type": "Point",
+        "coordinates": [7.678285, 46.68891]
+      },
+      "properties": {
+        "date": "2026-01-25",
+        "name": "Felder",
+        "type": "Food"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
         "coordinates": [-0.59075, 51.51056]
       },
       "properties": {
