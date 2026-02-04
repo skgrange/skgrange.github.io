a40f661cf369a274c4f1f2af1e600e051969b002
Stuart Grange <s.k.grange@gmail.com>
2026-02-04 19:34:31 +0100

    commit all website changes with a script

--- www/maps/location_tracking/data/points_done.js
+++ www/maps/location_tracking/data/points_done.js
@@ -62405,6 +62405,18 @@ var points_done = [{
       "type": "Feature",
       "geometry": {
         "type": "Point",
+        "coordinates": [2.164122, 48.71117]
+      },
+      "properties": {
+        "date": "2026-02-04",
+        "name": "bap meokja",
+        "type": "Food"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
         "coordinates": [-0.59075, 51.51056]
       },
       "properties": {
