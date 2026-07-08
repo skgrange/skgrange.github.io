daa4a321f787efba06ace13d1f707993d84f2afa
skgrange <s.k.grange@gmail.com>
2026-07-08 17:19:03 +0200

    commit all website changes with a script

--- www/maps/location_tracking/data/points_done.js
+++ www/maps/location_tracking/data/points_done.js
@@ -67673,6 +67673,30 @@ var points_done = [{
       "type": "Feature",
       "geometry": {
         "type": "Point",
+        "coordinates": [7.468069, 46.91454]
+      },
+      "properties": {
+        "date": "2026-07-08",
+        "name": "Velonova",
+        "type": "Shop"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [7.413779, 46.92261]
+      },
+      "properties": {
+        "date": "2026-07-08",
+        "name": "Gemeindehaus Bläuacker",
+        "type": "Other"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
         "coordinates": [-0.59075, 51.51056]
       },
       "properties": {
