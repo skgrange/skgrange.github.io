8ff2556395596669bee093a598c5bbaca6168e80
skgrange <s.k.grange@gmail.com>
2026-09-12 12:16:55 +0200

    commit all website changes with a script

--- www/maps/location_tracking/data/points_done.js
+++ www/maps/location_tracking/data/points_done.js
@@ -69317,6 +69317,18 @@ var points_done = [{
       "type": "Feature",
       "geometry": {
         "type": "Point",
+        "coordinates": [7.470513, 46.91093]
+      },
+      "properties": {
+        "date": "2026-09-12",
+        "name": "Coop",
+        "type": "Supermarket"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
         "coordinates": [-0.59075, 51.51056]
       },
       "properties": {
