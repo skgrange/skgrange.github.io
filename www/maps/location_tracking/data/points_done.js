5f55594f60aa093248543b22c8be174c4fa190bc
skgrange <s.k.grange@gmail.com>
2026-09-17 20:04:57 +0200

    commit all website changes with a script

--- www/maps/location_tracking/data/points_done.js
+++ www/maps/location_tracking/data/points_done.js
@@ -69629,6 +69629,18 @@ var points_done = [{
       "type": "Feature",
       "geometry": {
         "type": "Point",
+        "coordinates": [13.19687, 55.7024]
+      },
+      "properties": {
+        "date": "2026-09-17",
+        "name": "Thai Thai Kitchen",
+        "type": "Food"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
         "coordinates": [-0.59075, 51.51056]
       },
       "properties": {
